1913e7dc236876842c75cef5b6dcd87ac6af601e
ranuranjan <ranuranjan25@gmail.com>
2024-02-07 00:08:19 +0530

    git commant

--- Starting java/src/Basic/First.java
+++ Starting java/src/Basic/First.java
@@ -6,3 +6,20 @@ public class First {
         System.out.println("hell");
     }
 }
+
+
+
+
+//echo "# Learning_java" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/RanuRanjan/Learning_java.git
+//git push -u origin main
+//
+//
+//
+//git remote add origin https://github.com/RanuRanjan/Learning_java.git
+//git branch -M main
+//git push -u origin main
