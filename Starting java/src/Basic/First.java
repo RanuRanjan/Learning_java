3698a00c5ed814bec1efe41ca75c5c92717d1443
ranuranjan <ranuranjan25@gmail.com>
2024-02-07 07:58:51 +0530

    data  types

--- Starting java/src/Basic/First.java
+++ Starting java/src/Basic/First.java
@@ -2,8 +2,36 @@ package Basic;
 
 public class First {
 
-    public static void main (String args[]){
+    public static void main (String[] args){
         System.out.println("hell");
+
+
+
+//        DataType :
+
+//       Int:
+            int rollNo=25;
+            System.out.println(rollNo);
+            rollNo =21;
+            System.out.println( rollNo);
+
+
+//       String:
+
+            String name="ram";
+            System.out.println(name);
+            name ="ram kumar";
+            System.out.println(name);
+
+//        Boolean:
+            Boolean genderMale=true;
+            System.out.println(genderMale);
+            genderMale=false;
+            System.out.println(genderMale);
+
+//        float
+             float totalMoney=23142353253f;
+             System.out.println(totalMoney);
     }
 }
 
@@ -16,7 +44,7 @@ public class First {
 //git commit -m "first commit"
 //git branch -M main
 //git remote add origin https://github.com/RanuRanjan/Learning_java.git
-//git push -u origin main
+//git push -u origin main.
 //
 //
 //
